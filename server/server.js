85c629080e20a4c02944b2a433482174743bfb97
Garvit Saxena <garvitsaxena06@gmail.com>
2020-10-10 18:27:56 +0530

    mongoDB connected

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config/config.env' })
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
