482742c652f1a6374240c9d688e860ea1306e9ac
Garvit Saxena <garvitsaxena06@gmail.com>
2020-10-14 09:04:01 +0530

    react-setup done

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <div className="row">
+        <div className="col-md-6">Hello</div>
+        <div className="col-md-6">World</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
